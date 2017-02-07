bf7780f7ff2ed06b6feb259bf757ea3ba28176a0
erniecho <erniecho@gmail.com>
2017-02-06 21:04:58 -0500

    Need to remember to check off Is Initial View Controller in the attributes inspector . You will get error message and blank screen on the app build. The original swift for the program was removed.

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Ernie Cho on 2/6/17.
+//  Copyright © 2017 ErnShu. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
